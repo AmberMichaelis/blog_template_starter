4b7d75d2de830507198964b9999c0bc160983770
Amber M <amberswieczkowski@pm.me>
2023-08-16 17:38:36 -0500

    Update apiClient

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -4,35 +4,35 @@ import { useEffect, useState } from 'react';
 import apiClient from '../services/api-client';
 import { CanceledError } from 'axios';
 
+// This interface is defined on https://api.rawg.io/docs/#operation/games_list. Parameters are listed under "results".
+interface BlogPost {
+    id: number;
+    name: string;
+}
+
+// This interface is defined on https://api.rawg.io/docs/#operation/games_list. Paramenters are listed under "results schema".
+interface FetchBlogResponse {
+    count: number;
+    results: BlogPost[];
+}
+
 const usePosts = () => {
   const [posts, setPost] = useState<BlogPost[]>([]);
   const [error, setError] = useState('');
-
-  // This interface is defined on https://api.rawg.io/docs/#operation/games_list. Parameters are listed under "results".
-  interface BlogPost {
-    id: number;
-    name: string;
-  }
-
-  // This interface is defined on https://api.rawg.io/docs/#operation/games_list. Paramenters are listed under "results schema".
-  interface FetchBlogResponse {
-    count: number;
-    results: BlogPost[];
-  }
 
   useEffect(() => {
     const controller = new AbortController();
 
     apiClient
-      .get<FetchBlogResponse>('/games') // this endpoint comes from the API
+      .get<FetchBlogResponse>('/games', { signal: controller.signal }) // the games endpoint comes from the API
       .then((res) => setPost(res.data.results))
       .catch((err) => {
         if (err instanceof CanceledError) return;
-        setError(err.message)
-    });
+        setError(err.message);
+      });
 
-      return () => controller.abort();
-  });
+    return () => controller.abort();
+  }, []);
 
   return { posts, error };
 };
