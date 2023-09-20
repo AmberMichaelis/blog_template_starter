5413120ed3634c211f0d5bcae2a695d93a2d86c9
Amber M <amberswieczkowski@pm.me>
2023-09-20 17:22:37 -0500

    Display topics

--- src/components/TopicList.tsx
+++ src/components/TopicList.tsx
@@ -1,16 +1,27 @@
 /** @format */
 
+import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
 import useTopics from '../hooks/useTopics';
+import getCroppedImageUrl from '../services/image-url';
 
 const TopicList = () => {
-  const { data } = useTopics()
-  
+  const { data } = useTopics();
+
   return (
-    <ul>
-      {data.map(topic => 
-        <li key={topic.id}>{topic.name}</li>
-      )}
-    </ul>
+    <List>
+      {data.map((topic) => (
+        <ListItem key={topic.id} paddingY='5px'>
+          <HStack>
+            <Image
+              boxSize='32px'
+              borderRadius={8}
+              src={getCroppedImageUrl(topic.image_background)}
+            />
+            <Text fontSize='lg'>{topic.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
   );
 };
 
--- src/hooks/useTopics.ts
+++ src/hooks/useTopics.ts
@@ -5,6 +5,7 @@ import useData from './useData';
 export interface Topic {
   id: number;
   name: string;
+  image_background: string;
 }
 
 const useTopics = () =>  useData<Topic>('/genres');
